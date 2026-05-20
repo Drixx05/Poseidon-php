94e1d43883103abf1f5147111e6a6e3ffa0d3306
Pierral87 <pa.lacaze1@gmail.com>
2026-05-20 16:31:54 +0200

    MySQL Acces, Prepare Complete, Fonction & Procedure Exercice Start

---
+++ S15-MySQL/04-fonctions/04-01-fonctions.sql
@@ -0,0 +1,92 @@
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------- FONCTIONS PREDEFINIES ------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+
+-- Ici quelques exemples de fonctions prédéfinies 
+
+USE bibliotheque;
+
+SELECT DATABASE(); -- Fonction indiquant le nom de la bdd actuellement sélectionnée
+
+SELECT LAST_INSERT_ID(); -- Le dernier id inséré dans la BDD (auto incrémenté) de la session actuelle
+                            -- Les ORM récents dans les framework back utilisent cette fonction lorsque l'on fait une insertion dans une table mais que l'on continue de manipuler la donnée
+                            -- Il sera toujours intéressant après une insertion de récupérer immédiatement l'id pour continuer à traiter l'élément justement au travers de son id 
+
+
+-- Fonctions de manipulation de string
+SELECT CONCAT("a","b","c"); -- Permet de concaténer pour mettre dans une seule colonne
+SELECT CONCAT_WS(" - ", "a","b","c"); -- Concaténation avec un séparateur (toujours dans une seule colonne)
+
+SELECT CONCAT_WS(" ", id_abonne, prenom) AS "liste" FROM abonne;
+
+SELECT SUBSTRING("bonjour", 4); -- Permet de couper une chaine
+
+SELECT REPLACE("www.coucou.com", "w", "W"); -- Remplace un string par un autre avec une chaine
+
+SELECT UPPER("Salut"); -- Mets tout en maj 
+
+-- Fonctions de manipulation de date 
+SELECT CURDATE(); -- La date du jour
+SELECT CURDATE() + 0; -- La date du jour
+
+SELECT CURTIME(); -- L'heure de l'instant
+SELECT NOW(); -- La date et l'heure
+SELECT CURRENT_TIMESTAMP(); -- La date et l'heure
+
+SELECT UNIX_TIMESTAMP(CURDATE()); -- Transforme une date en timestamp 
+
+SELECT DATE_ADD(CURDATE(), INTERVAL 7 DAY); -- Pour ajouter un intervale de temps à une date (pourquoi pas une date limite d'emprunt sur la bibliotheque ?)
+SELECT DATE_ADD(CURDATE(), INTERVAL 2 MONTH);
+SELECT DATE_ADD(CURDATE(), INTERVAL 1 YEAR);
+
+SELECT DAYNAME(CURDATE()); -- Le nom du jour
+
+SELECT CURDATE();
+SELECT DATE_FORMAT(CURDATE(), "%d-%m-%Y");
+-- DATE_FORMAT nous permet de formater un type DATE/DATETIME en MySQL, cela nous permet de l'afficher en jour / mois / année plutot que annee - mois - jour 
+-- Cela fonctionne avec des tokens de remplacement, voir doc !  https://sql.sh/fonctions/date_format
+
+
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------- FONCTIONS UTILISATEURS -----------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+-- Fonctions que nous developpons nous même
+-- Une fonction nous permet d'effectuer un traitement en particulier 
+-- En MySQL une fonction nous retourne une valeur 
+
+DELIMITER $ -- On doit changer le delimiter (le point virgule) car je vais avoir besoin de mettre des points virgules dans mes fonctions, donc pour éviter qu'il pense que je suis à la fin de l'instruction 
+
+CREATE FUNCTION calcul_tva(nb INT) RETURNS TEXT -- On reçoit un param INT et on précise que le return sera du string 
+COMMENT "Fonction permettant le calcul de la TVA" 
+READS SQL DATA -- Indique simplement qu'on veut lire les données 
+    BEGIN 
+        RETURN CONCAT_WS(": ", "Le resultat est ", (nb*1.20));
+    END $ 
+
+DELIMITER ; 
+SELECT calcul_tva(100);
+
+-- Si on a besoin d'une variable dans une fonction on peut la déclarer avec DECLARE
+-- Par exemple : DECLARE mavar INT  
+    -- On peut insérer dans une variable le résultat d'une requête par exemple : SELECT xxxxx INTO mavar FROM xxxxxx WHERE xxxxxxx  
+
+    -- Les FLAGS que l'on peut indiquer en début d'une fonction 
+    -- NO SQL : Indique qu'une fonction ne contient pas d'instructions SQL 
+    -- CONTAINS SQL : Indique que la fonction contient des instructions SQL qui n'effectuent ni lecture ni modification 
+    -- READS SQL DATA : Indique que la fonction contient des instructions SELECT
+    -- MODIFIES SQL DATA : Indique que la fonction contient des instructions INSERT, UPDATE ou DELETE 
+    -- DETERMINISTIC: Spécifie qu'une fonction renvoie toujours le même résultat pour les mêmes paramètres d'entrée.
+    -- NOT DETERMINISTIC: Spécifie qu'une fonction peut renvoyer des résultats différents à chaque exécution, même pour les mêmes paramètres d'entrée (par exemple, si elle utilise des fonctions aléatoires).
+    -- SQL SECURITY : On peut définir le SQL SECURITY sur deux valeurs différentes (visible dans PHP My Admin) 
+        -- DEFINER : Pour indiquer que ce sont les droits d'accès de la personne qui a défini la fonction qui vont s'appliquer lors de l'appel de cette fonction
+        -- INVOKER : Pour indiquer sur ce sont les droits d'accès de la personne qui exécute la fonction qui vont s'appliquer 
+            -- Utile si on a une fonction dont le comportement diffère en fonction des droits des utilisateurs 
+
+  -- EXERCICE 1 : Le même calcul de TVA avec le choix du taux
+  -- EXERCICE 2 : Faire une fonction qui me retourne le nombre d'employés pour un service envoyé en param de la fonction 
---
+++ S15-MySQL/04-fonctions/04-02-procedure.sql
@@ -0,0 +1,48 @@
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------- PROCEDURE STOCKEE ----------------------------------------------------------
+--------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------
+
+-- Une procédure est très similaire à une fonction
+-- En MySQL la différence réside dans le fait qu'une fonction retourne une simple valeur alors que la procédure retourne un jeu de résultat de requête 
+
+-- Les avantages d'une procédure stockée 
+    -- Moins de risque de se tromper : plus facile de lancer une procédure nommée par exemple selectAll(), plutôt qu'à chaque fois de réécrire une requête SELECT avec une grosse jointure
+    -- Meilleure compréhension : plus facile de lire le nom de la procédure plutôt qu'une requête entière 
+    -- Facilité d'utilisation : Si d'autres personnes manipulent la BDD, même sans connaissances avancées en SQL, ils pourront comprendre facilement l'exécution d'une procédure 
+    -- Evolutivité : Si la requête est amenée à changer, elle ne sera modifiée qu'à un seul endroit, dans le SGBD et pas besoin de modifier tous les endroits du back qui l'appelle 
+    -- Sécurité : On laisse le travail à une seule personne : "La personne qui gère la BDD"
+    -- Optimisation : La procédure se lance plus rapidement qu'une requête à la main car l'analyse et l'interprétation de la requête sont déjà effectuées 
+
+
+DELIMITER $  
+
+----------------------------------
+-- Création d'une procédure stockée qui affiche la date du jour en français 
+CREATE PROCEDURE date_du_jour()
+BEGIN 
+    SELECT DATE_FORMAT(CURDATE(), "%d/%m/%Y") AS "today";
+END $ 
+CALL date_du_jour() $
+
+
+-- On peut avoir des params entrant/sortant d'une procédure 
+    -- IN : entrant 
+    -- OUT : sortant 
+    -- INOUT : les deux 
+
+    CREATE PROCEDURE addition(IN valeur1 INT, IN valeur2 INT)
+    BEGIN 
+        SELECT valeur1+valeur2;
+    END$ 
+
+    CALL addition(17,3) $
+
+-- - Exercice 1/ Faire une procédure stockée qui affiche toutes les informations de tous les employes
+-- - Exercice 2/ Faire une PROCEDURE qui prends en param le prenom d'un employe et qui affiche le service et le salaire de l'employé
+-- - Exercice 3/ Cette année, chaque salarié va toucher 10% de son salaire en plus et une prime de 700€. Faite une procédure permettant de calculer le nouveau salaire annuel de chaque salarié et de le modifier. Le but étant d'appeler la procédure pour un salarié à la fois
+-- - Exercice 4/ Faire une procédure qui prends en param le prénom et indique de quel groupe il fait parti parmis les groupes suivant 
+                        -- Plus de 3000e = Groupe 1 
+                        -- Entre 2000 et 3000 = Groupe 2 
+                        -- Entre 0 et 2000 = Groupe 3 
