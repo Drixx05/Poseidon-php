735710c1f258829c47291f65ea1b9e593247fdcd
Pierral87 <pa.lacaze1@gmail.com>
2026-06-24 15:02:56 +0200

    Chapter 7 Exception Exercices Start

--- S16-PHPOO/07-stdCLass-exit-Exception/3-Exception.php
+++ S16-PHPOO/07-stdCLass-exit-Exception/3-Exception.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 /* 
@@ -19,3 +19,57 @@
     finally : Est un bloc que l'on peut rajouter après le try/catch qui s'exécutera quoi qu'il en soit, que l'on soit passé dans le catch ou pas 
 
 */
+
+
+// On dev une fonction qui a un cas d'erreur défini
+// Si on tombe dans ce cas d'erreur, on lancera une exception ! 
+
+function diviser($a, $b)
+{
+    // cette fonction va faire une division de a par b
+    // On connait un cas d'erreur, celui d'une division par zero impossible ! 
+
+    if ($b == 0) {
+        // Si je tombe dans mon cas d'erreur ici, je décide de générer une erreur de type "Exception"
+        // Exception fait parti de la famille des throwable 
+        // On les instancie d'une manière spécifique, avec throw, cela va permettre de faire fonctionner le try/catch
+        throw new Exception("Division par zéro interdite ! ");
+    }
+    return $a / $b;
+}
+
+echo diviser(10, 5); // Ici ok c'est bon
+echo "<hr>";
+// echo diviser(10, 0); // Ici cas d'erreur division par 0 ! 
+// Si je suis dans le global scope, alors, un lancement d'exception = fatal error ! 
+
+// Le but de l'exception pour nous, est d'être géré ! Au travers des bloc try/catch, pour ne pas forcément laisser une fatal error, mais faire apparaitre un message d'erreur plus clair 
+
+// je lance mes blocs try/catch
+// Dans le bloc try, je peux éxécuter tout le code que je souhaite, si tout va bien, alors pas de problème, on exécute le bloc try en entier et on poursuit notre script
+try {
+    echo "<h2>Je suis dans le bloc try ! </h2>";
+
+    echo diviser(10, 5); // Ici ok c'est bon
+    echo diviser(10, 2); // Ici ok c'est bon
+    echo "<p>Coucou</p>";
+
+    // echo diviser(10, 0); // Cette ligne lance l'exception, comme je suis dans un try/catch, alors je vais atterrir dans le catch ! 
+
+    echo "<hr>";
+} catch (Exception $e) {
+    // Si j'arrive ici c'est qu'une exception a été throw par une instruction dans le try !
+    // Auquel cas je récupère l'objet Exception (ça dépends de ce que j'ai défini en param du bloc catch, ici je défini que j'attrape une Exception classique)
+
+    // Il existe d'autres types d'exception, je peux faire plusieurs catch si nécessaire si je gère ces différents types 
+
+    echo "<h2>Je suis dans le bloc catch ! </h2>";
+    // var_dump($e);
+    // var_dump(get_class_methods($e));
+    echo "Erreur : " . $e->getMessage() . " - déclenchée par : " . $e->getTraceAsString() . "<hr>";
+} finally { // Le bloc finally s'exécute quoi qu'il en soit 
+    echo "<h2>Je suis le finally ! Je m'exécute quoi qu'il arrive ! </h2>";
+}
+
+//  Grace à la bonne gestion de l'exception au travers des blocs try/catch, il m'est possible de poursuivre le code ! Contrairement à un fatal error et une exception non caught qui stoppe directement l'execution du code 
+echo "<h2>Je suis après le bloc try/catch ! </h2>";
