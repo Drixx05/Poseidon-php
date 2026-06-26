525efe6776b8b27d83b429bff58a0b5a7cdb1fac
Drixx05 <drixion05@gmail.com>
2026-06-26 10:01:20 +0200

    finalized Clean archi DDD for HPP TP

--- S16-PHPOO/10-TP-Transformation/public/index.php
+++ S16-PHPOO/10-TP-Transformation/public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 session_start();
 
@@ -13,6 +14,7 @@ use \App\Auth\Infrastructure\Controllers\RegisterUserController;
 use App\Chat\Infrastructure\Repositories\PdoMessageRepository;
 use App\Chat\Application\UseCases\PostMessage;
 use App\Chat\Infrastructure\Controllers\ChatController;
+use App\Auth\Infrastructure\Controllers\LogoutController;
 
 require __DIR__ . '../../autoload.php';
 
@@ -27,6 +29,7 @@ $registerController = new RegisterUserController($registerUser);
 $messageRepository = new PdoMessageRepository($pdo);
 $postMessage       = new PostMessage($messageRepository);
 $chatController    = new ChatController($postMessage, $messageRepository);
+$logoutController  = new LogoutController();
 
 $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $route = str_replace('/Poseidon-php/S16-PHPOO/10-TP-Transformation/public', '', $route);
@@ -35,5 +38,6 @@ match ($route) {
     '/register' => $registerController->handle(),
     '/login'    => $loginController->handle(),
     '/chat'     => $chatController->handle(),
+    '/logout'   => $logoutController->handle(),
     default     => http_response_code(404),
 };
